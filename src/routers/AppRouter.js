630332cf5ae9232d7f2a26d7993b6973be2c695e
Shivam Satyarthi <shivamsatyarthi@Shivams-MBP.fios-router.home>
2020-05-17 01:20:51 -0400

    removed unnecessary files

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,22 +1,10 @@
-import React from "react";
-import { BrowserRouter, Route, Switch, Link, NavLink } from "react-router-dom";
-import Header from "../components/Header.component";
-import Footer from "../components/Footer.component";
-import Cover from "../components/cover/";
-import theme from '../styles/theme'
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/styles'
-import { CssBaseline } from '@material-ui/core'
-import { Typography } from '@material-ui/core'
+import { CssBaseline, Typography } from '@material-ui/core'
 import Splash from '../containers/Splash';
 import Dashboard from '../containers/Dashboard';
-/*
-import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
-import AddExpensePage from '../components/AddExpensePage';
-import EditExpensePage from '../components/EditExpensePage';
-import HelpPage from '../components/HelpPage';
-import NotFoundPage from '../components/NotFoundPage';
-import Header from '../components/Header';
-*/
+import theme from '../styles/theme';
 
 class AppRouter extends React.Component {
 
@@ -25,22 +13,15 @@ class AppRouter extends React.Component {
   }
 
   render() {
-
     return (
-
       <BrowserRouter>
         <React.Fragment>
           <CssBaseline />
           <ThemeProvider theme={theme}>
-
               <Switch>
                 <Route exact={true} path="/Dashboard" component={Dashboard}/>
                 <Route exact={true} path="/" component={Splash} />
               </Switch>
-              
-  
-       
-
           </ThemeProvider>
         </React.Fragment>
        </BrowserRouter>
